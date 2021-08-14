c8fcfb398e2a52fbb46322a69b9f46376e660bf2
sepam <mwieriks@gmail.com>
2021-08-14 14:28:47 +0700

    add datasource links

--- data/get_data.py
+++ data/get_data.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+"""
+List of data sources by province: https://docs.google.com/spreadsheets/d/1ma1T9hWbec1pXlwZ89WakRk-OfVUQZsOCFl4FwZxzVw/edit#gid=1814790353
+
+"""
+
+
 
 URL = 'https://tiny.cc/Datacovidjakarta'
 SHEET = 'Data Indonesia dan Jakarta'
