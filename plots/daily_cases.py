487182994ab8202a06c7ffef3a11609bc0b0135e
sepam <mwieriks@gmail.com>
2021-08-14 15:05:34 +0700

    Add: Vertical ruler and tooltip

--- plots/daily_cases.py
+++ plots/daily_cases.py
@@ -23,4 +23,42 @@ def daily_cases(df, region):
             alt.Tooltip(region_col, format=',', title='Daily Cases')
         ],
     )
-    return chart
+
+    # Create a selection that chooses the nearest point & selects based on x-value
+    nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                        fields=['Tanggal'], empty='none')
+
+    # Transparent selectors across the chart. This is what tells us
+    # the x-value of the cursor
+    selectors = alt.Chart(df).mark_point().encode(
+        x='Tanggal',
+        opacity=alt.value(0),
+    ).add_selection(
+        nearest
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = chart.mark_point(color='#F63366').encode(
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+    )
+
+    # Draw text labels near the points, and highlight based on selection
+    text = chart.mark_text(align='left', dx=-30, dy=-25, color='#F63366').encode(
+        text=alt.condition(nearest, region_col, alt.value(' '))
+    )
+
+    # Draw a rule at the location of the selection
+    rules = alt.Chart(df).mark_rule(color='#F63366').encode(
+        x='Tanggal',
+    ).transform_filter(
+        nearest
+    )
+
+    # Put the layers into a single chart and bind the data
+    layered_chart = alt.layer(
+        chart, selectors, points, rules, text
+    ).properties(
+        width=600, height=300
+    )
+
+    return layered_chart
